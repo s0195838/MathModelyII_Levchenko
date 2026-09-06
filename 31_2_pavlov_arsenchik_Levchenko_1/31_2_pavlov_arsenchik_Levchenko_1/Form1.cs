9b2480fa6352ca1fd6f6d4c76483de8c10ca2844
s0195838 <pavlart55@gmail.com>
2026-09-06 13:09:21 +0300

    Add files via upload

---
+++ 31_2_pavlov_arsenchik_Levchenko_1/31_2_pavlov_arsenchik_Levchenko_1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _31_2_pavlov_arsenchik_Levchenko_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).BackColor == Color.Navy)
+            {
+                ((Button)sender).BackColor = Color.Black;  //меняем цвет кнопки.
+            }
+            else
+            {
+                ((Button)sender).BackColor = Color.Navy;
+
+            }    
+        }
+    }
+}
---
+++ 31_2_pavlov_arsenchik_Levchenko_1/31_2_pavlov_arsenchik_Levchenko_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("31_2_pavlov_arsenchik_Levchenko_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("31_2_pavlov_arsenchik_Levchenko_1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1e42310e-d6ae-4daf-90a4-451343a13ccf")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
